6ff210099caba20e2ab3d7b43459398bfb2fc492
Henrique Luiz <heenluy@gmail.com>
2024-01-07 16:40:34 -0300

    change color theme

--- app/src/main/java/me/henriquelluiz/courses/ui/theme/Color.kt
+++ app/src/main/java/me/henriquelluiz/courses/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package me.henriquelluiz.courses.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val onBackgroundLight = Color(0xFF050316)
+val backgroundLight = Color(0xFFfbfbfe)
+val primaryLight = Color(0xFF2f27ce)
+val secondaryLight = Color(0xFFdddbff)
+val tertiaryLight = Color(0xFF443dff)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val backgroundDark = Color(0xFF010104)
+val onBackgroundDark = Color(0xFFeae9fc)
+val primaryDark = Color(0xFF3a31d8)
+val secondaryDark = Color(0xFF020024)
+val tertiaryDark = Color(0xFF0600c2)
